537774c00c0847e077480faccb7a765655218a17
Reza_Bamoniri <rezabri806@gmail.com>
2026-08-16 17:47:32 +0330

    feat: add scroll to comments feature

--- src/components/product/HeroDetails/elements/UsefulButtons/FocusOnComments.tsx
+++ src/components/product/HeroDetails/elements/UsefulButtons/FocusOnComments.tsx
@@ -1,12 +1,31 @@
+import { useEffect } from "react";
 import { FaComment } from "react-icons/fa";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
+
 import Tooltip from "../../../../common/tooltips/Tooltip";
 
 
 const FocusOnComments = () => {
+
+  const location = useLocation()
+  
+  useEffect(() => {
+    
+  const hash = location.hash;
+  if(hash){
+    const commentElem = document.querySelector(hash);
+
+    commentElem?.scrollIntoView({behavior:"smooth"})
+  }
+    
+    
+  }, [location])
+  
+  
+
   return (
     <Tooltip text={"User Reviews"}>
-      <Link to="" className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-sky-600">
+      <Link to="#comments-container" className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-sky-600">
         <FaComment />
       </Link>
     </Tooltip>
