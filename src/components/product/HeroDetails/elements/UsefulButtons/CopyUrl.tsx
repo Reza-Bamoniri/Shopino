b98e8a54232fbfaeddc98a6872f5a9854c4f0d05
Reza_Bamoniri <rezabri806@gmail.com>
2026-08-16 17:08:50 +0330

    feat: add copy url handler

--- src/components/product/HeroDetails/elements/UsefulButtons/CopyUrl.tsx
+++ src/components/product/HeroDetails/elements/UsefulButtons/CopyUrl.tsx
@@ -1,12 +1,26 @@
 import { FaShare } from "react-icons/fa";
+import { useState } from "react";
+
 import Tooltip from "../../../../common/tooltips/Tooltip";
 
 
 
 const CopyUrl = () => {
+
+  const [copied,setCopied] = useState(false)
+
+  const handleCopyUrl = () => {
+    const pruductUrl = location.origin + location.pathname;
+    navigator.clipboard.writeText(pruductUrl);
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 800);
+  };
+
   return (
-    <Tooltip>
-      <button className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-slate-700">
+    <Tooltip text={copied ? "copied" : "copy"}>
+      <button onClick={handleCopyUrl} className="cursor-pointer size-8 text-sm hover:*:text-lg *:duration-150 *:transition-all active:*:text-sm flex-center border border-neutral-300 rounded-lg text-slate-700">
         <FaShare />
       </button>
     </Tooltip>
